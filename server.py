7043f3afec20b60471d17eba886b4f75f388b11d
ssdsangavi0709-ui <ssdsangavi0709@gmail.com>
2026-03-14 23:49:42 +0530

    Added admin button

--- server.py
+++ server.py
@@ -2,7 +2,11 @@ import json
 import os
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 from flask_cors import CORS
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
